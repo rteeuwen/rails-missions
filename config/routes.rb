5ba34e18bd8a2e13697164bfa40f10bb74d3f91a
Richard Teeuwen <rt@penthion.nl>
2022-10-20 15:44:06 +0200

    after mission 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :logs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post "/fibonacci.json", to: "fibonacci#show"
+
 end
